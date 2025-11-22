c3bb85b698f3c2d118d2c233d503d1492a5dced0
Jo Ayeon <dddx0208@naver.com>
2025-11-22 22:52:12 +0900

    added sprint box

--- lib/screens/todo_list_screen.dart
+++ lib/screens/todo_list_screen.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:week_plan/components/color_manage.dart';
-import 'dart:async';
 
-import 'package:week_plan/components/icon_manage.dart';
 import 'package:week_plan/components/widgets/view_slider.dart';
 import 'package:week_plan/widgets/todo_list/add_button.dart';
 import 'package:week_plan/widgets/todo_list/instructor.dart';
+import 'package:week_plan/widgets/todo_list/sprint_box.dart';
 import 'package:week_plan/widgets/todo_list/todo_card.dart';
 
 class TodoListScreen extends ConsumerWidget {
@@ -17,7 +15,6 @@ class TodoListScreen extends ConsumerWidget {
   Scaffold build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
             children: [MyCustomSlider()],
@@ -28,9 +25,10 @@ class TodoListScreen extends ConsumerWidget {
                 Instructor(),
                 SizedBox(width: 214),
                 Column(
+                  spacing: 15,
                   children: [
                     AddButton(),
-                    SizedBox(height: 15),
+                    SprintBox(),
                     TodoCard(title: 'title', category: 'category'),
                   ],
                 ),
---
+++ lib/widgets/todo_list/sprint_box.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:week_plan/components/color_manage.dart';
+import 'package:week_plan/widgets/todo_list/todo_card.dart';
+
+class SprintBox extends StatelessWidget {
+  const SprintBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: 4,
+        horizontal: 6,
+      ),
+      decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(width: 3, color: AppColors.cyan(2))),
+      child: Column(
+        children: [
+          TodoCard(title: 'title', category: 'cate'),
+        ],
+      ),
+    );
+  }
+}
