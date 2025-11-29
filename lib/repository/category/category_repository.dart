fec5b67bed537215e2dea646151f1d6c4184b2a7
Jo Ayeon <dddx0208@naver.com>
2025-11-29 21:18:40 +0900

    made a codes to add category to firebase

---
+++ lib/repository/category/category_repository.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:week_plan/models/category/category_model.dart';
+
+class CategoryRepository {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Stream<List<CategoryModel>> streamCategories(String uid) {
+    return firestore
+        .collection('categories')
+        .where('uid', isEqualTo: uid)
+        .snapshots()
+        .map((snapshot) {
+      return snapshot.docs
+          .map(
+            (doc) => CategoryModel.fromJson({
+              ...doc.data(),
+            }),
+          )
+          .toList();
+    });
+  }
+
+  Future<void> addCategory(CategoryModel todo) async {
+    final docRef = firestore.collection('categories').doc();
+    final newTodo = todo.copyWith(categoryId: docRef.id);
+
+    await docRef.set(newTodo.toJson());
+  }
+
+  Future<void> deleteCategory(String id) async {
+    await firestore.collection('categories').doc(id).delete();
+  }
+}
